Fix image normalization by converting torchvision implementation of normalize to a numpy-only method in lambda function. Numpy uses considerably less space than torchvision.

Lambda_predict/lambda_predict.py
@@ -28,19 +28,11 @@ PREDICT_PATH = '/predict'
 INFO_PATH = '/info'
 
 
-def normalize_image(image_bytes):
-    # Convert bytes (buffer) to 3-channels, then to ndarray
-    # We could do this without PIL (using only NumPy)
-    pil_image = Image.open(image_bytes).convert('RGB') # pil_image.size (224, 224) with 3 channels
-    # print('pil_image dimensions', pil_image.size)
-    np_image = np.array(pil_image, dtype=np.float32) # np_image.shape (224, 224, 3) np_image.dtype float32
-    # print('np_image shape', np_image.shape)
-    # print('np_image dtype', np_image.dtype)
-
+def standardize_image(np_image):
     # Convert lists to numpy
-    np_mean = np.array(TRAIN_MEAN, dtype=np.float32).reshape(1, 1, 3) # np_mean.shape (1, 1, 3)
+    np_mean = np.array(TRAIN_MEAN, dtype=np.float32).reshape(3, 1, 1) # np_mean.shape (3, 1, 1)
     # print('np_mean shape', np_mean.shape)
-    np_std = np.array(TRAIN_STD, dtype=np.float32).reshape(1, 1, 3) # np_std.shape (1, 1, 3)
+    np_std = np.array(TRAIN_STD, dtype=np.float32).reshape(3, 1, 1) # np_std.shape (3, 1, 1)
 
     # Normalize
     # Operations are performed element-wise using NumPy broadcasting
@@ -50,21 +42,30 @@ def normalize_image(image_bytes):
 
 def preprocess(image_filename):
     # Download image (png file) as bytes from S3
+    print('Loading', S3_BUCKET, image_s3key)
     image_s3key = os.path.join(S3_ORIGINALS_DIR, image_filename)
-    print('Loading', image_s3key, 'from', S3_BUCKET)
-
     s3 = boto3.client('s3')
     file_obj = s3.get_object(Bucket=S3_BUCKET, Key=image_s3key)
     image_bytes = BytesIO(file_obj['Body'].read())
-    preprocessed_np_image = normalize_image(image_bytes)
 
-    preprocessed_flattened = np.ravel(preprocessed_np_image)  # ndarray shape (150528,)
-    # print('preprocessed_flattened shape', preprocessed_flattened.shape)
-    return preprocessed_flattened
+    # Convert bytes (buffer) to 3-channels, then to ndarray
+    # We could do this without PIL (using only NumPy)
+    pil_image = Image.open(image_bytes).convert('RGB') # pil_image.size (224, 224) with 3 channels
+    # print('pil_image dimensions', pil_image.size)
+    np_image = np.array(pil_image, dtype=np.float32) # np_image shape (224, 224, 3) dtype float32
+    # print('np_image shape', np_image.shape, 'dtype', np_image.dtype)
+    transposed_np = np.transpose(np_image, (2, 0, 1)) # shape (3, 224, 224) max pixel value = 255.
+    # print('transposed_np shape', transposed_np.shape, 'max pixel value =', np.max(transposed_np))
+    normalized_np = transposed_np / 255.0 # normalize range to [0., 1.]
+    # print('normalized_np image shape', normalized_np.shape, 'max pixel value =', np.max(normalized_np))
+    standardized_np = standardize_image(normalized_np) # normalize color-channels
+    # print('standardized_np image shape', standardized_np.shape)
+    flattened_np = np.ravel(standardized_np)  # ndarray shape (150528,)
+    # print('flattened_np image numpy shape', flattened_np.shape)
+    return flattened_np
 
 
 def sigmoid(np_outs):
-    # Clip the values to a reasonable range
     np_outs = np.clip(np_outs, -50, 50) # prevent np.exp overflow for large values
     return 1 / (1 + np.exp(-np_outs))
 

utilities.py
@@ -12,6 +12,7 @@ from io import BytesIO
 from PIL import Image
 import torch
 from torchvision import transforms
+import numpy as np
 
 # To download 977 thumbnails from S3
 import boto3
@@ -61,7 +62,7 @@ def download_latest_model():
 
 
 def preprocess(image_url):
-    # print('image_url', image_url)
+    print('image_url', image_url)
     with urllib.request.urlopen(image_url) as response:
         image_data = response.read()
     image_file = BytesIO(image_data)
@@ -73,14 +74,18 @@ def preprocess(image_url):
     # Model expects the input to be ndarray (150528,)
     train_mean = [0.738, 0.649, 0.775]
     train_std =  [0.197, 0.244, 0.17]
+
+    # torchvision.transforms.ToTensor: Converts a PIL Image or
+    # numpy.ndarray (H x W x C) in the range [0, 255] to a
+    # torch.FloatTensor of shape (C x H x W) in the range [0.0, 1.0]
     val_MHIST_FCN_transforms = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize(train_mean, train_std),
-        transforms.Lambda(lambda x: torch.flatten(x))
+        transforms.Normalize(train_mean, train_std), # Channels must be dim 0
+        transforms.Lambda(lambda x: torch.flatten(x)) # [3, 224, 224] to [150528]
     ])
 
     preprocessed_image = val_MHIST_FCN_transforms(image_PIL).numpy() # ndarray shape (150528,)
-    # print('preprocessed_image shape', preprocessed_image.shape)
+    print('Flattened preprocessed_image numpy shape', preprocessed_image.shape)
     return preprocessed_image
 
 
@@ -112,3 +117,41 @@ def predict(image_url, ort_session): # <class '_io.BytesIO'>
         'predicted_class': 'SSA' if positive_class else 'HP'
         })
     return json_info
+
+
+def get_PILs(filename):
+    S3_URL_ORIGINALS = "https://mhist-streamlit-app.s3.us-west-1.amazonaws.com/images/test-set/original/"
+    S3_BUCKET = "mhist-streamlit-app"
+    S3_ORIGINALS_DIR = "images/test-set/original/"
+    s3 = boto3.client('s3')
+
+    # Copied from above (it was correct when I used it in Streamlit)
+    image_url = S3_URL_ORIGINALS+filename
+    print('URL Lib download: \n image_url', image_url)
+    with urllib.request.urlopen(image_url) as response:
+        image_data = response.read()
+    image_file = BytesIO(image_data)
+    img1 = Image.open(image_file).convert('RGB') # PIL Image size (224, 224)
+
+    # Used this code in Lambda
+    image_s3key = os.path.join(S3_ORIGINALS_DIR, filename)
+    print('Loading png from S3', image_s3key, 'from', S3_BUCKET)
+    file_obj = s3.get_object(Bucket=S3_BUCKET, Key=image_s3key)
+    image_bytes = BytesIO(file_obj['Body'].read())
+    img2 = Image.open(image_bytes).convert('RGB') # pil_image.size (224, 224) with 3 channels
+
+    return img1, img2
+
+def compare_PILs(img1=None, img2=None): # requires two PIL images, ex: img1 = Image.open(img1_path)
+    if img1 is None and img2 is None:
+        TEST_FILENAME = "MHIST_aah.png"
+        img1, img2 = get_PILS(TEST_FILENAME)
+
+    if img1.size != img2.size:
+        print('images are different sizes')
+        return False
+
+    print('Testing pixel by pixel:')
+    arr1 = np.array(img1)
+    arr2 = np.array(img2)
+    return np.array_equal(arr1, arr2)